dd9704551984cb8a2500ab967d0931f57cc2725d
DESKTOP-2PSOO5E\Nidoran <seanm07@googlemail.com>
2024-01-21 22:25:22 +0000

    Project switched to class library Console dependency removed Added file utility to copy a directory

--- FileUtil.cs
+++ FileUtil.cs
@@ -18,4 +18,37 @@ public class FileUtil {
         // file is not locked
         return false;
     }
+
+    public static bool CopyDirectory(string sourceDir, string destDir, bool overwriteExisting = true) {
+        try {
+            // Get the subdirectories for the specified directory
+            string[] subDirectories = Directory.GetDirectories(sourceDir);
+
+            foreach (string subDir in subDirectories) {
+                // Create the corresponding subdirectory in the destination directory
+                string subDirName = Path.GetFileName(subDir);
+                string newSubDir = Path.Combine(destDir, subDirName);
+
+                Directory.CreateDirectory(newSubDir);
+
+                // Recursively copy the subdirectory
+                CopyDirectory(subDir, newSubDir);
+            }
+
+            // Get all files in the source directory
+            string[] files = Directory.GetFiles(sourceDir);
+
+            // Copy each file to the destination directory
+            foreach (string file in files) {
+                string fileName = Path.GetFileName(file);
+                string destinationPath = Path.Combine(destDir, fileName);
+
+                File.Copy(file, destinationPath, overwriteExisting);
+            }
+        } catch (IOException) {
+            return false;
+        }
+
+        return true;
+    }
 }
